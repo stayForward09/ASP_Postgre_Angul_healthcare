b27c2d98865d9e5311547f2157bc7b5953f0e95d
AnichoKraus <anichokraus3123@gmail.com>
2021-06-08 20:09:08 -0400

    system health cards created

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Server} from '../../shared/server'
+
+const SAMPLE_SERVERS = [
+  {id: 1, name: 'dev-web', isOnline: true},
+  {id: 2, name: 'dev-mail', isOnline: false},
+  {id: 3, name: 'prod-web', isOnline: true},
+  {id: 4, name: 'prod-mail', isOnline: true}
+]
+
+
 @Component({
   selector: 'app-section-health',
   templateUrl: './section-health.component.html',
@@ -8,6 +18,8 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
+
+  servers: Server[] = SAMPLE_SERVERS;
 
   ngOnInit(): void {
   }
